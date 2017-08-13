bb900285dd796e2abaa06cf845f4e9fc0f784acf
Andrew Carpenter <andrew@audioinhd.com>
2017-08-12 23:35:59 -0400

    Make compatible with > node 6

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 const jszip = require('jszip')
 const matcher = /<w:bookmarkStart.+?(?:(?:(?:w:id="(.+?)").+?(?:w:name="(.+?)"))|(?:(?:w:name="(.+?)").+?(?:w:id="(.+?)"))).+?<w:bookmarkEnd.+?\/>/g
 const defTags = '<w:r><w:t></w:t></w:r>'
+
+// helpers
 const getType = (o) => {
   if (typeof o === 'string') return 'base64'
   if (typeof Buffer === 'function' && o instanceof Buffer) return 'nodebuffer'
@@ -16,27 +18,35 @@ const getType = (o) => {
   return 'blob'
 }
 
-// main
-module.exports = async (docx, marks) => {
-  let type = getType(docx)
-  let zip = await jszip.loadAsync(docx, {base64: type === 'base64'})
-  let files = Object.keys(zip.files).filter((k) => k.match(/^word\/.+\.xml$/))
-  for (let f of files) {
-    let text = await zip.file(f).async('string')
-    text = text.replace(matcher, (match, id, name, name2, id2) => {
-      id = id || id2
-      name = name || name2
-      if (!marks[name]) return match
-      let wrap = (match.match(/(<w:r[> ].+<\/w:r>)/) || [])[0] || defTags
-      let [raw, val] = (wrap.match(/<w:t(?:>|\s.+?>)(.*)<\/w:t>/) || []) || []
-      val = !val ? '' : val.replace(/<.+?>/g, '')
-      val = (typeof marks[name] === 'function') ? marks[name](val) : marks[name]
-      let start = `<w:bookmarkStart w:id="${id}" w:name="${name}"/>`
-      let content = `<w:t xml:space="preserve">${val}</w:t>`
-      let end = `<w:bookmarkEnd w:id="${id}"/>`
-      return `${start}${wrap.replace(raw, content)}${end}`
-    })
-    zip.file(f, text)
+const getReplacer = (marks) => {
+  return (match, id, name, name2, id2) => {
+    id = id || id2
+    name = name || name2
+    if (!marks[name]) return match
+    let wrap = (match.match(/(<w:r[> ].+<\/w:r>)/) || [])[0] || defTags
+    let [raw, val] = (wrap.match(/<w:t(?:>|\s.+?>)(.*)<\/w:t>/) || []) || []
+    val = !val ? '' : val.replace(/<.+?>/g, '')
+    val = (typeof marks[name] === 'function') ? marks[name](val) : marks[name]
+    let start = `<w:bookmarkStart w:id="${id}" w:name="${name}"/>`
+    let content = `<w:t xml:space="preserve">${val}</w:t>`
+    let end = `<w:bookmarkEnd w:id="${id}"/>`
+    return `${start}${wrap.replace(raw, content)}${end}`
   }
-  return await zip.generateAsync({type})
+}
+
+// main
+module.exports = (docx, marks) => {
+  let type = getType(docx)
+  let replacer = getReplacer(marks)
+  return jszip.loadAsync(docx, {base64: type === 'base64'}).then((zip) => {
+    let files = Object.keys(zip.files).filter((k) => k.match(/^word\/.+\.xml$/))
+    let replace = (f) => {
+      return zip.file(f).async('string').then((text) => {
+        text = text.replace(matcher, replacer)
+        zip.file(f, text)
+        return Promise.resolve()
+      })
+    }
+    return Promise.all(files.map(replace)).then(() => zip.generateAsync({type}))
+  })
 }
